da5ad21263f3dae26f78365ad9ddd25bb7f974f2
MK-726 <245698842+MK-726@users.noreply.github.com>
2026-06-14 20:23:57 +0330

    add time related types

--- src/types/primitives.ts
+++ src/types/primitives.ts
@@ -9,9 +9,20 @@ export type ID = string;
 
 // ------ Currency ------
 export type CurrencyCode = "IRR" | "USD" | "EUR" | "AED" | "GBP" | "TRY" |
-    "CAD" | "AUD"
+    "CAD" | "AUD" | "JPY"
 
 export interface Money {
     amount: number,
     currency: CurrencyCode;
+}
+
+// ------ Time ------
+export type ISODateString = string; // "2025-06-14"
+export type ISODateTimeString = string; // "2025-06-14T12:00:00Z"
+
+export type TimeHorizon = "short" | "medium" | "long";
+
+export interface DateRange {
+    from: ISODateString;
+    to: ISODateString;
 }
