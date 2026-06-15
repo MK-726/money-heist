d6edf3e9469bc07f0bc1ae2d6708ba30e68000eb
MK-726 <245698842+MK-726@users.noreply.github.com>
2026-06-15 21:53:27 +0330

    bundle store functionality

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+export { Store } from "./Store";
+export type { Listener, Selector } from "./Store";
+export type { AppState } from "./AppState";
+export { appStore } from "./appStore";
+export * from "./selectors";
