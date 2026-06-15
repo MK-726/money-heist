eb8c58344e421583293a233901d7d86f8583c4ed
MK-726 <245698842+MK-726@users.noreply.github.com>
2026-06-15 22:19:36 +0330

    bundle router function and values

---
+++ src/core/index.ts
@@ -0,0 +1,4 @@
+export { Router } from "./Router";
+export type { FeatureModule } from "./FeatureModule";
+export { routes, defaultRoute, matchRoute } from "./routes";
+export type { Route, RouteKey } from "./routes";
